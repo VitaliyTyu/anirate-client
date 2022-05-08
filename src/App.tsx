ba2dad4ae1bf5096c08400e4982b8a4523f1c575
Егор Белов <e1belov@yandex.ru>
2022-05-08 13:38:42 +0500

    Добавление компонентов для глав страницы

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React, {useEffect, useState} from 'react';
 import AnimeList from "./components/AnimeList";
 import AnimeTitle from "./components/AnimeTitle";
+import Slider from './components/Slider/Slider';
+import HeaderNotAuthorize from './components/HeaderNotAuthorize/HeaderNotAuthorize';
+import HeaderNotAuthorize1 from './components/HeaderNotAuthorize1/HeaderNotAuthorize1'
+import MainPage from './components/MainPage/MainPage';
+
+
 
 const App = () => {
   return (
       <div>
-          <AnimeTitle/>
-          <hr/>
-          <AnimeList/>
+           {/* {/* <AnimeTitle/>
+              <hr/>
+             <AnimeList/> 
+            <Slider/> */}
+             <HeaderNotAuthorize1/>
+             <MainPage/>
+             <Slider/>
       </div>
   );
 };
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer" >
+            <div className="container">                
+                
+                <div className="copyright">
+                    © 2022 AniRate  by <span>LOL</span>
+                </div>
+    
+            </div>
+        </footer>
+    );
+};
+
+export default Footer; 
